a6359272fb5aa6db99e150230b15899ac167ab4b
Matteo Marchelli <matteo.marchelli@gmail.com>
2025-11-06 13:32:53 +0000

    added some default for my own sanity (#2)

--- build.sh
+++ build.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
 function log() {
-  echo "===== $(date) ==> ${*}"
+	echo "===== $(date) ==> ${*}"
 }
 
 log "Running go vet"
 if ! go vet ./...; then
-  echo "go vet failed"
-  exit 1
+	echo "go vet failed"
+	exit 1
 fi
 echo
 
 shopt -s nullglob
 set -- *_test.go
 if [ "$#" -gt 0 ]; then
-  log "Running go test"
-  if ! go test -v; then
-    echo "go test failed"
-    exit 1
-  fi
+	log "Running go test"
+	if ! go test -v; then
+		echo "go test failed"
+		exit 1
+	fi
 fi
 shopt -u nullglob
 echo
 
 if type -p govulncheck >/dev/null; then
-  log "Running govulncheck"
-  govulncheck ./...
+	log "Running govulncheck"
+	govulncheck ./...
 fi
 echo
 
 if type -p golangci-lint >/dev/null; then
-  log "Running golangci-lint"
-  if ! golangci-lint run -E revive -E errcheck -E nilerr -E gosec -E staticcheck -E prealloc; then
-    echo "Check above..."
-  fi
+	log "Running golangci-lint"
+	if ! golangci-lint run -E revive -E errcheck -E nilerr -E gosec -E staticcheck -E prealloc -E nilerr -E gochecksumtype; then
+		echo "Check above..."
+	fi
 fi
 echo
 
@@ -53,13 +53,13 @@ archs=(amd64 arm64)
 log "building..."
 
 for GOOS in "${oses[@]}"; do
-  for GOARCH in "${archs[@]}"; do
-    echo "Building ${GOARCH} for ${GOOS}..."
-    # shellcheck disable=SC2097,SC2098
-    GOOS=${GOOS} GOARCH=${GOARCH} CGO_ENABLED=0 \
-      go build -ldflags "-s -w -X 'main.Version=v${version}'" \
-      -o "bin/${name}-${GOOS}-${GOARCH}" .
-  done
+	for GOARCH in "${archs[@]}"; do
+		echo "Building ${GOARCH} for ${GOOS}..."
+		# shellcheck disable=SC2097,SC2098
+		GOOS=${GOOS} GOARCH=${GOARCH} CGO_ENABLED=0 \
+			go build -ldflags "-s -w -X 'main.Version=v${version}'" \
+			-o "bin/${name}-${GOOS}-${GOARCH}" .
+	done
 done
 
 echo
